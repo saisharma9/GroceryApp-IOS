a8309735a5e1e63083615b0a74755c8277d63c00
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-04-12 10:48:00 -0500

    Add files via upload

---
+++ Mantha_GroceryApp/Mantha_GroceryApp/GrocerySectionsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mantha_GroceryApp
+//
+//  Created by Mantha,Sai Sharma on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var grocerySectionsTableView: UITableView!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mantha_GroceryApp/itemInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  itemInfoViewController.swift
+//  Mantha_GroceryApp
+//
+//  Created by Mantha,Sai Sharma on 4/5/22.
+//
+
+import UIKit
+
+class itemInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
